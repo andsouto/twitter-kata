c01a40dce577b07b17f8238df2a90d8114cc450f
Andrés Souto <kai670@gmail.com>
2020-10-07 19:35:57 +0200

    initial commit

---
+++ src/company.service.ts
@@ -0,0 +1,13 @@
+export class CompanyService {
+
+	// Collaborators(?)
+
+	addEmployee(companyId: unknown, employeeId: unknown): void {
+		// Employees should not be duplicated.
+	}
+
+	deleteEmployee(employeeId: unknown): void {
+		// When deleting an employee, all the bookings and policies associated to the employee should also be deleted from the system.
+	}
+
+}
